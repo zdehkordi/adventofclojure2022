cbf610f1469a62ebae31b330bf3fe95b711c765f
Zac Dehkordi <zac.dehkordi@gmail.com>
2022-12-21 09:00:05 -0500

    init commit

---
+++ day01/spec/day01/core_spec.clj
@@ -0,0 +1,7 @@
+(ns day01.core-spec
+  (:require [speclj.core :refer :all]
+            [day01.core :refer :all]))
+
+(describe "a test"
+  (it "FIXME, I fail."
+    (should= 0 1)))
---
+++ day01/src/day01/core.clj
@@ -0,0 +1,5 @@
+(ns day01.core)
+
+(defn -main
+  [& args]
+  (println "Hello World"))
