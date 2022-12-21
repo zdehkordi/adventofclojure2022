865168d4148fda5330317f429876d46b64450fdd
Zac Dehkordi <zac.dehkordi@gmail.com>
2022-12-21 09:22:43 -0500

    copy uncle bob haha

--- day01/spec/day01/core_spec.clj
+++ day01/spec/day01/core_spec.clj
@@ -1,7 +1,13 @@
 (ns day01.core-spec
-  (:require [speclj.core :refer :all]
-            [day01.core :refer :all]))
+  (:require [speclj.core :refer [describe, it, should=]]
+            [day01.core :refer [parse-inventory]]
+            [clojure.string :as string]))
 
-(describe "a test"
-  (it "FIXME, I fail."
-    (should= 0 1)))
+(describe "Parse Input"
+          (it "reads file"
+              (should= "1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000"
+                       (slurp "sample-input")))
+
+          (it "breaks input into lines"
+              (should= ["this" "that" "" "the" "other"] (string/split-lines "this\nthat\n\nthe\nother")))
+          (it "parses input" (should= [[1000 2000 3000] [4000] [5000 6000] [7000 8000 9000] [10000]] (parse-inventory "sample-input"))))
--- day01/src/day01/core.clj
+++ day01/src/day01/core.clj
@@ -1,5 +1,14 @@
-(ns day01.core)
+(ns day01.core
+  (:require clojure.string :as string) )
 
-(defn -main
-  [& args]
-  (println "Hello World"))
+(defn parse-inventory [file-name]
+  (loop [lines (string/split-lines (slurp file-name))
+    input [[]]
+    elf 0]
+  (if (empty? lines)
+    input
+    (let [line (first lines)]
+      (if (empty? line)
+        (recur (rest lines) (conj input []) (inc elf))
+        (let [calories (Integer/parseInt line)]
+          (recur (rest lines) (update-in input [elf] conj calories) elf)))))))
